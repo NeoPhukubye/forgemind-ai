e984d04f7b8425e89775abfb4222a24d430e21fb
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2026-07-23 18:03:56 +0200

    progress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import PlannerPage from "./pages/PlannerPage";
+import CoderPage from "./pages/CoderPage";
+import DebuggerPage from "./pages/DebuggerPage";
+import TesterPage from "./pages/TesterPage";
+import DocsPage from "./pages/DocsPage";
 
 export default function App() {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-
     return (
         <>
-            <Navbar onMenuClick={() => setSidebarOpen((v) => !v)} />
-            <Home sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/planner" element={<PlannerPage />} />
+                <Route path="/coder" element={<CoderPage />} />
+                <Route path="/debugger" element={<DebuggerPage />} />
+                <Route path="/tester" element={<TesterPage />} />
+                <Route path="/docs" element={<DocsPage />} />
+            </Routes>
             <Footer />
         </>
     );
